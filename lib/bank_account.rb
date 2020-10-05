696908d1a623e7cd740c1a0eb721b7234245dac2
Djura22 <michaeliangroves@gmail.com>
2020-10-05 11:42:24 +0100

    Build: account history now recorded with instance array, tested for and passed

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,14 +1,20 @@
 class BankAccount
 
-  attr_reader :balance
+  attr_reader :balance, :account_history
 
   def initialize
     @balance = 0
+    @account_history = []
   end
 
-  def deposit(amount)
+  def deposit(amount, date)
     @balance += amount
+    @account_history << {date => amount}
   end
- 
+
+  def withdraw(amount, date)
+    @balance -= amount
+    @account_history << {date => -amount}
+  end
 
 end  
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -2,21 +2,33 @@ require 'bank_account'
 
 describe BankAccount do
 
+  before do
+    @acc = BankAccount.new
+    @acc.deposit(10, '10/12/2019')
+  end
+
+  it 'stores each transaction with the history array' do
+    expect(@acc.account_history).to eq(['10/12/2019' => 10])
+  end
+
   describe 'balance' do
-
     it 'returns the balance of the account' do
-      expect(subject.balance).to eq(0)
+      expect(@acc.balance).to eq(10)
     end
-
-  end  
+  end
 
   describe 'deposit' do
-
     it 'tops up the account balance' do
-      subject.deposit(10)
-      expect(subject.balance).to eq(10)
-    end  
+      @acc.deposit(10, '12/12/2019')
+      expect(@acc.balance).to eq(20)
+    end
+  end
 
+  describe 'withdraw' do
+    it 'when called it reduces balance by withdrawal amount' do
+      @acc.withdraw(10, '12/12/2019')
+      expect(@acc.balance).to eq(0)
+    end
   end
 
 end
