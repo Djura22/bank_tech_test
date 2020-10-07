6edad214b8692f9ca5bd4052f87c76c7064d349b
Djura22 <michaeliangroves@gmail.com>
2020-10-07 18:03:31 +0100

    Testing: bank_account - transactionhistory successfully mocked. Feature test ongoing

--- spec/bank_account_feature_spec.rb
+++ spec/bank_account_feature_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'bank_account'
+require 'transaction_history'
 
 describe BankAccount do
 
@@ -10,7 +11,7 @@ describe BankAccount do
 
   describe 'print_statement' do
     it 'when called prints statement of account history' do
-      allow(transaction).to receive(:transact_history).and_return([{:date=>time, :credit=> "10.00", :debit=>" ", :balance=>"10.00"}])
+      allow(transaction).to receive(:transact_history).and_return([{:date=>time, :credit=>"10.00", :debit=>" ", :balance=>"10.00"}])
       expect(acc.print_statement).to include(balance: '10.00', credit: '10.00', date: time, debit: ' ')
     end
   end
